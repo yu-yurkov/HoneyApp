a72f247aee26008fb518e6b45c9875c4627074e3
Yuri Yurkov <yu.yurkov@gmail.com>
2018-08-11 17:14:28 +1000

    Корзина изменение количества +- подсчет суммы заказа выбор магазина

--- app/src/main/java/com/example/honeyapp/activities/OrderPlace.java
+++ app/src/main/java/com/example/honeyapp/activities/OrderPlace.java
@@ -5,7 +5,11 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.example.honeyapp.R;
@@ -16,6 +20,7 @@ import com.example.honeyapp.database.App;
 import com.example.honeyapp.database.AppDatabase;
 import com.example.honeyapp.entities.UserCartEntity;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.TreeMap;
 
@@ -25,6 +30,10 @@ public class OrderPlace extends AppCompatActivity{
 
     private List<UserCartEntity> userCart;
     RecyclerView recyclerView;
+
+    public static TextView userCartSumm;
+    public static Spinner spinner;
+
 
 
     @Override
@@ -46,19 +55,46 @@ public class OrderPlace extends AppCompatActivity{
         setContentView(R.layout.user_cart_order_place);
 
 
-        TextView cartText = findViewById(R.id.user_cart_order_place);
-        cartText.setText("Товаров в корзине: " + userCart.size());
-
         recyclerView = findViewById(R.id.cart_recyclerView);
+
+        // сумма покупок
+        userCartSumm = findViewById(R.id.user_cart_summ);
+        cartRefresh(userCart);
+
+        // магазины
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            list.add(String.valueOf(i)+" магазин");
+        }
+
+
+        spinner = findViewById(R.id.stores);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, list);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
 
         OrderPlaceAdapter myAdapter = new OrderPlaceAdapter(this, userCart);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
         recyclerView.setAdapter(myAdapter);
-
 
     }
 
+    public void orderSend(View v){
+        Log.i("TAG", "Выбрана позиция: " +spinner.getSelectedItemId());
+    }
+
+    // пересчитываем товары в корзине
+    public static void cartRefresh(List<UserCartEntity> userCart){
+
+        float summ = 0;
+
+        for (int i = 0; i < userCart.size(); i++) {
+            summ += userCart.get(i).quantity * Float.parseFloat(userCart.get(i).products.getPrice());
+        }
+
+        userCartSumm.setText(String.valueOf(summ));
+    }
 
 
     @Override
--- app/src/main/java/com/example/honeyapp/adapters/OrderPlaceAdapter.java
+++ app/src/main/java/com/example/honeyapp/adapters/OrderPlaceAdapter.java
@@ -2,18 +2,28 @@ package com.example.honeyapp.adapters;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
 import com.example.honeyapp.R;
+import com.example.honeyapp.dao.UserCartDao;
+import com.example.honeyapp.dao.UserCartDao_Impl;
+import com.example.honeyapp.database.App;
+import com.example.honeyapp.database.AppDatabase;
 import com.example.honeyapp.entities.UserCartEntity;
+import com.example.honeyapp.model.UserCart;
 
 import java.util.List;
+
+import static com.example.honeyapp.activities.OrderPlace.cartRefresh;
+
 
 public class OrderPlaceAdapter extends RecyclerView.Adapter<OrderPlaceAdapter.MyViewHoder> {
 
@@ -34,8 +44,7 @@ public class OrderPlaceAdapter extends RecyclerView.Adapter<OrderPlaceAdapter.My
 
         View view;
         LayoutInflater inflater = LayoutInflater.from(mContext);
-        view = inflater.inflate(R.layout.cardview_item, parent,false);
-
+        view = inflater.inflate(R.layout.user_cart_item, parent,false);
 
 
         final OrderPlaceAdapter.MyViewHoder myViewHoder = new OrderPlaceAdapter.MyViewHoder(view);
@@ -48,11 +57,10 @@ public class OrderPlaceAdapter extends RecyclerView.Adapter<OrderPlaceAdapter.My
 
         holder.tv_name.setText(userCart.get(position).products.getName());
         holder.tv_price.setText(userCart.get(position).products.getPrice());
+        holder.tv_quantity.setText(String.valueOf(userCart.get(position).quantity));
 
         // загрузка изображения
         Glide.with(mContext).load(userCart.get(position).products.getPhoto()).apply(option).into(holder.img_photo);
-        //holder.tv_name.setText("Name");
-        //holder.tv_price.setText("99");
 
     }
 
@@ -61,11 +69,20 @@ public class OrderPlaceAdapter extends RecyclerView.Adapter<OrderPlaceAdapter.My
         return userCart.size();
     }
 
-    public class MyViewHoder extends RecyclerView.ViewHolder {
+    public class MyViewHoder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
         TextView tv_name;
         TextView tv_price;
+        TextView tv_quantity;
         ImageView img_photo;
+        Button plusBtn;
+        Button minusBtn;
+
+        public AppDatabase db;
+        public UserCartDao userCartDao;
+        public UserCartEntity userCartEntity;
+
+
 
         public MyViewHoder(View itemView) {
             super(itemView);
@@ -73,6 +90,49 @@ public class OrderPlaceAdapter extends RecyclerView.Adapter<OrderPlaceAdapter.My
             tv_name = itemView.findViewById(R.id.name);
             tv_price = itemView.findViewById(R.id.price);
             img_photo = itemView.findViewById(R.id.photo);
+            tv_quantity = itemView.findViewById(R.id.item_quantity);
+
+
+            plusBtn = itemView.findViewById(R.id.plusButton);
+            minusBtn = itemView.findViewById(R.id.minusButton);
+
+            plusBtn.setOnClickListener(this);
+            minusBtn.setOnClickListener(this);
+
+            // бд
+            db = App.getInstance().getDatabase();
+            userCartDao = db.userCartDao();
+
+        }
+
+        @Override
+        public void onClick(View v) {
+
+            // получаем текущее количесво
+            int item_quantity = userCart.get(getAdapterPosition()).quantity;
+
+
+            switch (v.getId()){
+
+                case R.id.plusButton:
+                    item_quantity++;
+                    break;
+
+                case R.id.minusButton:
+                    if(item_quantity > 1) item_quantity--;
+                    break;
+
+            }
+
+
+            // обновляем
+            userCart.get(getAdapterPosition()).quantity = item_quantity; // обновляем массив
+            userCartDao.update(userCart.get(getAdapterPosition())); // записываем в корзину
+
+            notifyDataSetChanged();
+
+            // показываем рассчеты
+            cartRefresh(userCart);
 
         }
     }
